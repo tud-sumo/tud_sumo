e57dc32ac2ba71aa6151c5b781e051c35bd40d2b
TUD-SUMO <c.evans@tudelft.nl>
2024-09-16 16:45:30 +0200

    Fixed __init__.py

--- tud_sumo/__init__.py
+++ tud_sumo/__init__.py
@@ -2,7 +2,6 @@ from tud_sumo.simulation import *
 from tud_sumo.utils import *
 from tud_sumo.controllers import VSLController, RGController
 from tud_sumo.events import EventScheduler, Event
-from tud_sumo.scenarios import sim_from_osm, add_sim_demand
 from tud_sumo.plot import Plotter
 
 __version__ = "3.1.0"
